c67b42d1db2a7636f984eced4dcca7bdd60aa18b
Thorsten <thorsten.elfert@gmail.com>
2013-11-13 00:02:13 +0100

    popup groesser

--- carsharing/src/main/resources/import.sql
+++ carsharing/src/main/resources/import.sql
@@ -50,3 +50,21 @@ INSERT INTO `participation` VALUES (1,0,0,'2013-11-12',1,1,'2013-11-19');
 -- INSERT INTO `participation` VALUES (10,0,0,'2013-03-11',0,6,'2013-10-29');
 -- INSERT INTO `participation` VALUES (11,0,0,'2013-03-11',0,7,'2013-10-29');
 -- INSERT INTO `participation` VALUES (12,0,0,'2013-03-11',0,8,'2013-10-29');
+
+-- Für openshift:
+-- INSERT INTO carsharing.`Member` (`id`,`carsize`,`city`,`firstname`,`name`) VALUES (0,4,'Grosskrotzenburg','Johann','A.');
+-- INSERT INTO carsharing.`Member` (`id`,`carsize`,`city`,`firstname`,`name`) VALUES (1,4,'Grosskrotzenburg','Moritz','E.');
+-- INSERT INTO carsharing.`Member` (`id`,`carsize`,`city`,`firstname`,`name`) VALUES (2,3,'Grosskrotzenburg','Julius','E.');
+-- INSERT INTO carsharing.`Member` (`id`,`carsize`,`city`,`firstname`,`name`) VALUES (3,3,'Grosskrotzenburg','Silas','N.');
+-- INSERT INTO carsharing.`Member` (`id`,`carsize`,`city`,`firstname`,`name`) VALUES (4,3,'Grosskrotzenburg','Simon','S.');
+-- INSERT INTO carsharing.`Member` (`id`,`carsize`,`city`,`firstname`,`name`) VALUES (5,3,'Grosskrotzenburg','Paul','S.');
+-- INSERT INTO carsharing.`Member` (`id`,`carsize`,`city`,`firstname`,`name`) VALUES (6,1,'Grosskrotzenburg','Robin','W.');
+-- INSERT INTO carsharing.`Member` (`id`,`carsize`,`city`,`firstname`,`name`) VALUES (7,1,'Grosskrotzenburg','Oliver','Z.');
+-- INSERT INTO carsharing.`Member` (`id`,`carsize`,`city`,`firstname`,`name`) VALUES (8,5,'Grosskrotzenburg','Lovis','W.');
+-- INSERT INTO carsharing.`Member` (`id`,`carsize`,`city`,`firstname`,`name`) VALUES (9,4,'Grosskrotzenburg','Paula','S.');
+-- INSERT INTO carsharing.`Member` (`id`,`carsize`,`city`,`firstname`,`name`) VALUES (10,1,'Grosskrotzenburg','Yunus','T.');
+-- INSERT INTO carsharing.`Member` (`id`,`carsize`,`city`,`firstname`,`name`) VALUES (11,1,'Grosskrotzenburg','Marie-Ann','W.');
+-- INSERT INTO carsharing.`Member` (`id`,`carsize`,`city`,`firstname`,`name`) VALUES (12,4,'Grosskrotzenburg','Peter','S.');
+-- INSERT INTO carsharing.`Member` (`id`,`carsize`,`city`,`firstname`,`name`) VALUES (13,4,'Grosskrotzenburg','Jon','G.');
+-- INSERT INTO carsharing.`TrainingDay` (`weekday`,`location`,`pickUpLocationSource`,`pickUpLocationTarget`,`pickUpTimeSource`,`pickUpTimeTarget`,`timeFrom`,`timeTo`) VALUES (3,'Grossauheim','Geschwister-Scholl Schule','Lindenauhalle','18:00','19:55','17:00','18:30');
+-- INSERT INTO carsharing.`TrainingDay` (`weekday`,`location`,`pickUpLocationSource`,`pickUpLocationTarget`,`pickUpTimeSource`,`pickUpTimeTarget`,`timeFrom`,`timeTo`) VALUES (4,'Grossauheim','Geschwister-Scholl Schule','Lindenauhalle','17:00','19:10','17:00','18:30');
